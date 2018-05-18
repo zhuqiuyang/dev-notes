1e61b9aadcfcbcd8408fd4e066d4b6c5ff99323a
zhuqiuyang <zqyhit@qq.com>
2018-05-18 11:39:57 +0800

    imd-ca test ok

--- blockChain/Fabric/CA/wait-for-mysql.sh
+++ blockChain/Fabric/CA/wait-for-mysql.sh
@@ -3,14 +3,15 @@
 
 set -e
 
-host="$1"
+host=`echo $1 | awk -F: '{print $1}'`
+port=`echo $1 | awk -F: '{print $2}'`
 shift
 cmd="$@"
 
-echo "${host} , cmd is ${cmd}"
+echo "${host} , ${port}, cmd is ${cmd}"
 
 # until mysql -h "$host" -u "root" -p "rootpw"; do
-until nc -z db 3306; do
+until nc -z $host $port; do
   >&2 echo "mysql is unavailable - sleeping"
   sleep 2
 done
@@ -18,3 +19,6 @@ done
 sleep 3
 >&2 echo "mysql is up - executing command"
 exec $cmd
+
+$(echo {$foo/:/ } | nc -z)
+$(echo 'db 3306' | nc -z)
